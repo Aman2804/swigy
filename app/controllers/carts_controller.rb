dee9f014431a7a069f93d9a56ce5ecfea5bdffa5
Aman2804 <hyeiamaman@gmail.com>
2019-06-28 03:46:23 +0530

    eleventh commit

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,6 +3,9 @@ class CartsController < ApplicationController
 		@restaurant = current_user.cart.cart_items.first.restaurant_item_id
 		@cart_items = current_user.cart.cart_items.all
 		@total = 0
-		@all_restaurant_id = current_user.cart.cart_items.pluck(:restaurant_item_id, :num_of_item)
+	end
+	def edit
+		@restaurant = current_user.cart.cart_items.first.restaurant_item_id
+		@cart_items = current_user.cart.cart_items.all
 	end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,32 @@
 class OrdersController < ApplicationController
 	def index
-		# @cart = 
+		@restaurant = Restaurant.find(params[:restaurant_id])
+		@orders = Order.all.select{|order| order.restaurant_id == @restaurant.id}
+		@order_items = @orders.map{|obj|p obj.id}.map{|order_id| OrderItem.all.select{|order_item| order_item.order_id == order_id }}.flatten 
 	end
 	def create
-		binding.pry
-		@order = current_user.orders.build(restaurant_id: params[:restaurant_id],status: "pending").save
-		params[:all_restaurant_id].each do |id|
+		@order = current_user.orders.create(restaurant_id: params[:restaurant_id],status: "pending")
+		current_user.cart.cart_items.pluck(:restaurant_item_id, :num_of_item).each do |id|
 			@order.order_items.create(restaurant_item_id: id.first,quantity: id.last)
 		end
-		redirect_to @order
+		current_user.cart.cart_items.destroy_all
+		redirect_to order_path(current_user,@order)
+	end
+	def update
+		@order = Order.find(params[:id])
+		@order.status = params[:value]
+		binding.pry
+		@user = params[:users_id]
+		@order.update(params.permit(:status))
+		redirect_to orders_path(params[:users_id],restaurant_id: @order.restaurant_id)
+
 	end
 	def show
 		@current_order = Order.find(params[:id])
 		@order_items = @current_order.order_items.all
+	end
+	private
+	def order_params
+		params.require(:order).permit(:restaurant_id,:status,:user_id)
 	end
 end
